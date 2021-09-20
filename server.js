d9687ad75dd4c94932cdd8e4cbdd0e84f2a03a96
essam omar <essam.93@hotmail.com>
2021-09-20 19:37:33 +0300

    lab12

--- server.js
+++ server.js
@@ -8,12 +8,13 @@ app.use(cors());
 const mongoose = require("mongoose");
 const MONGO_SERVER = process.env.MONGO_SERVER;
 const PORT = process.env.PORT || 8000;
-const booksController = require("./controllers/Book.controller")
+const {booksController,createBook} = require("./controllers/Book.controller")
 const {bookModel} = require("./models/Book.model");
 
 
 app.listen(PORT, () => console.log(`listening on ${PORT}`));
 
 app.get('/books', booksController)
+app.post('/creat-book',createBook)
 
 mongoose.connect(`${MONGO_SERVER}/Book`,{useNewUrlParser: true, useUnifiedTopology: true});
