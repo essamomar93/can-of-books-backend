352885abf95af1883d0eb9c8c5929b9ea3ad1f16
DimaAlabsi <dema.yaser8@gmail.com>
2021-09-20 18:02:53 +0300

    lab11

--- server.js
+++ server.js
@@ -9,14 +9,24 @@ app.use(cors());
 const mongoose=require("mongoose");
 const MONGO_SERVER=process.env.MONGO_SERVER;
 
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT ;
+// || 8000;
 const booksController=require("./controllers/Book.controller")
 const {
    bookModel}=require("./models/Book.model");
 
+// app.get('/books', (req, res) => {
+//   // seedBook();
+//   // res.json({'message':'test request received'})
 
+// })
 app.listen(PORT, () => console.log(`listening on ${PORT}`));
 
 app.get('/books',booksController)
 
+
+// app.get('/books',(req,res)=>{
+//   bookModel.find().then(data=>{
+//       res.json(data);
+// })})
 mongoose.connect(`${MONGO_SERVER}/Book`,{useNewUrlParser: true, useUnifiedTopology: true});
